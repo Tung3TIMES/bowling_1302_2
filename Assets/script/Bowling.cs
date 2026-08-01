0ca252a2522574dae459fb8ce69fd3a3e1b301e8
Tung3TIMES <sing.num42@gmail.com>
2026-08-01 14:13:56 +0700

    commit

--- Assets/script/Bowling.cs
+++ Assets/script/Bowling.cs
@@ -28,7 +28,7 @@ public class Bowling : MonoBehaviour
 
     }
     
-    private void ShootBall()
+    public void ShootBall()
     {
         rb.AddForce(Vector3.forward * forcePower, ForceMode.Impulse);
     }
