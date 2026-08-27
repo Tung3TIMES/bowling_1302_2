1cb81a2694be0b51eae48ccf2484c8f74354caab
delta030737-pixel <delta030737@gmail.com>
2026-08-28 01:08:46 +0700

    bug fix || main menu || restart system

--- Assets/script/Bowling.cs
+++ Assets/script/Bowling.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
 public class Bowling : MonoBehaviour
 {
     [SerializeField]
@@ -8,13 +10,11 @@ public class Bowling : MonoBehaviour
     [SerializeField]
     private int forcePower;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Keyboard.current.spaceKey.wasPressedThisFrame)
@@ -22,12 +22,16 @@ public class Bowling : MonoBehaviour
 
         if (Keyboard.current.rightArrowKey.isPressed)
             MoveRight();
-        
+
         if (Keyboard.current.leftArrowKey.isPressed)
             MoveLeft();
 
+        if (Keyboard.current.rKey.wasPressedThisFrame)
+        {
+            RestartGame();
+        }
     }
-    
+
     public void ShootBall()
     {
         rb.AddForce(Vector3.forward * forcePower, ForceMode.Impulse);
@@ -41,5 +45,11 @@ public class Bowling : MonoBehaviour
     private void MoveLeft()
     {
         transform.position += new Vector3(-2f, 0f, 0f) * Time.deltaTime;
+    }
+
+    public void RestartGame()
+    {
+        string currentSceneName = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(currentSceneName);
     }
 }
--- Assets/script/MainMenu.cs
+++ Assets/script/MainMenu.cs
@@ -1,19 +1,17 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void PlayGame()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
-        
+        Debug.Log("Quit Game!");
+        Application.Quit();
     }
-
-
-
 }
+
